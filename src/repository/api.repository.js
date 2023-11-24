fe9feaabe8c5d967b0175504b57db6491964a621
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-11-24 17:27:22 +0300

    TaskManager-Server

--- src/repository/api.repository.js
+++ src/repository/api.repository.js
@@ -2,21 +2,40 @@ const pool = require('../db');
 
 async function getUserByEmail(email) {
     const client = await pool.connect();
-    const sql = `SELECT * FROM users WHERE email =$1`;
+    try {
+        await client.query('BEGIN');
 
-    const data = (await client.query(sql, [email])).rows;
+        const sql = `SELECT * FROM users WHERE email =$1`;
 
-    return data;
+        const data = (await client.query(sql, [email])).rows;
+        await client.query('COMMIT');
+
+        return data;
+    } catch (error) {
+        await client.query('ROLLBACK');
+        console.log(`getUserByEmail: ${error.message}`);
+
+        return [];
+    }
 }
 
 async function createUserDB(name, surname, email, hashPwd) {
     const client = await pool.connect();
-    const sql = `INSERT INTO users(name, surname, email, pwd) 
+    try {
+        await client.query('BEGIN');
+        const sql = `INSERT INTO users(name, surname, email, pwd) 
     VALUES($1, $2, $3, $4) RETURNING *`;
 
-    const data = (await client.query(sql, [name, surname, email, hashPwd])).rows;
+        const data = (await client.query(sql, [name, surname, email, hashPwd])).rows;
+        await client.query('COMMIT');
 
-    return data;
+        return data;
+    } catch (error) {
+        await client.query('ROLLBACK');
+        console.log(`createUser: ${error.message}`);
+
+        return [];
+    }
 }
 
 module.exports = { getUserByEmail, createUserDB };
